05589ce24da3ec2f7929a730a1dd8f535e52d8ec
peh <philipp@errbuddy.net>
2015-04-15 16:17:35 +0200

    optimized worker startup a bit

--- grails-app/services/grails/plugin/uberjobs/UberWorkerService.groovy
+++ grails-app/services/grails/plugin/uberjobs/UberWorkerService.groovy
@@ -9,20 +9,23 @@ class UberWorkerService extends AbstractUberService {
     def uberWorkerMetaService
 
     def createWorkersFromConfig() {
+        // iterate over workers configurations
         config.workers.each { poolName, config ->
+            List queueNames
+            // check queue configuration, should be a list, a single string or a closure that is returning a list
+            if (config.queueNames in List) {
+                queueNames = config.queueNames
+            } else if (config.queueNames instanceof Closure) {
+                def closureResult = config.queueNames.call()
+                assert closureResult in List, "$poolName's queueNames closure has not returned a list!"
+                queueNames = closureResult
+            } else if (config.queueNames in String) {
+                queueNames = [config.queueNames]
+            } else {
+                throw new RuntimeException("queueNames of $poolName must be a List, Closure (returning a list) or a String. ${config.queueNames.class.simpleName} is not allowed!")
+            }
+            // start the appropiate amount of workers
             config.workers.times { index ->
-                List queueNames
-                if (config.queueNames in List) {
-                    queueNames = config.queueNames
-                } else if (config.queueNames instanceof Closure) {
-                    def closureResult = config.queueNames.call()
-                    assert closureResult in List, "$poolName's queueNames closure has not returned a list!"
-                    queueNames = closureResult
-                } else if (config.queueNames in String) {
-                    queueNames = [config.queueNames]
-                } else {
-                    throw new RuntimeException("queueNames of $poolName must be a List, Closure (returning a list) or a String. ${config.queueNames.class.simpleName} is not allowed!")
-                }
                 start(poolName, index, queueNames)
             }
         }
