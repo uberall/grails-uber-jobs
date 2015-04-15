8b9e51b820d6561f343e93ae34a3729f6a390adb
peh <philipp@errbuddy.net>
2015-04-15 10:28:32 +0200

    updated worker meta info

--- grails-app/domain/grails/plugin/uberjobs/UberWorkerMeta.groovy
+++ grails-app/domain/grails/plugin/uberjobs/UberWorkerMeta.groovy
@@ -1,7 +1,65 @@
 package grails.plugin.uberjobs
 
+import org.joda.time.DateTime
+
+/**
+ * Worker meta information
+ */
 class UberWorkerMeta {
 
+    /**
+     * the hostname this worker is working on
+     */
+    String hostname
+
+    /**
+     * the pool that this worker belongs to
+     */
+    String poolName
+
+    /**
+     * the index inside the pool (needed for determining the name)
+     */
+    int index
+
+    /**
+     * the status of this worker
+     */
+    Status status
+
+    /**
+     * create and update times
+     */
+    DateTime dateCreated, lastUpdated
+
     static constraints = {
+        hostname nullable: false
+        poolName nullable: false
+        index min: 1
+    }
+
+    /**
+     * Status information for workers
+     */
+    enum Status {
+        /**
+         * Worker is doing nothing as no work has to be done
+         */
+        IDLE,
+
+        /**
+         * Worker is working
+         */
+        WORKING,
+
+        /**
+         * Worker is paused
+         */
+        PAUSED,
+
+        /**
+         * Worker was killed
+         */
+        KILLED
     }
 }
